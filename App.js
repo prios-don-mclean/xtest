0220124e78a70298bd1d710b5f39c189c08cd487
Don Mclean <don.mclean@principles.com>
2019-01-15 17:29:31 -0500

    change bg to blue

--- App.js
+++ App.js
@@ -34,7 +34,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'red',
+    backgroundColor: 'blue',
   },
   welcome: {
     fontSize: 20,
