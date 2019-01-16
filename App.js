57f4cedad3f944a27d0991ca19054545bcc6f5a1
Don Mclean <don.mclean@principles.com>
2019-01-16 14:05:53 -0500

    change bg top white

--- App.js
+++ App.js
@@ -34,7 +34,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'green',
+    backgroundColor: 'white',
   },
   welcome: {
     fontSize: 20,
