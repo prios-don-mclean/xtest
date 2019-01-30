f59e670c9ea91ebbb205b6386028256d0d7f712c
Don Mclean <don.mclean@principles.com>
2019-01-30 15:03:00 -0500

    update test code

--- tests/test1.js
+++ tests/test1.js
@@ -89,82 +89,21 @@ describe('Test that app loads', function() {
             assert.equal(browser.isIOS, true);
         }
     });
+
+    it('change welcomeText', async function() {
+        // const selector = 'new UiSelector().index(1)';
+        // const TestView = await browser.$(`android=${selector}`);
+        if (browser.isAndroid) {
+            //select by accessibilityLabel (android)
+            const welcomeTextEl = await browser.$('~welcomeText');
+
+            const welcomeText = await welcomeTextEl.getText();
+    
+            assert.equal(welcomeText, 'Welcome to React Native ON YTEST!');
+        }
+    
+        if (browser.isIOS) {
+            assert.equal(browser.isIOS, true);
+        }
+    });
 });
-
-// (async () => {
-//     const browser = await remote({
-//         logLevel: 'error',
-//         path: '/',
-//         capabilities: {
-//             browserName: 'firefox'
-//         }
-//     });
-
-//     await browser.url('https://webdriver.io');
-
-//     const title = await browser.getTitle();
-//     console.log('Title was: ' + title);
-
-//     await browser.deleteSession();
-// })().catch((e) => console.error(e));
-
-
-
-
-// const wd = require('wd');
-// const assert = require('assert');
-
-// const SERVER_PORT = 4723;
-// const SERVER_URL = 'localhost';
-// const CAPABILITIES = {
-//     platformName: 'Android',
-//     deviceName: 'Android Emulator',
-//     app: process.cwd() + "/android/app/build/outputs/apk/release/app-release.apk"
-// };
-
-// const driver = wd.promiseChainRemote(SERVER_URL, SERVER_PORT);
-
-// describe('Test that app loads', function() { 
-//     // this.timeout(300000);  
-//     before(async function() {
-//         try {
-//             this.timeout(10000);
-//             await driver.init(CAPABILITIES);
-//         } catch(err) {
-//             if(err && err.message) {
-//                 console.log(err.message);
-//             } else {
-//                 console.log(err);
-//             }
-//         }
-//     });
-
-//     after(async () => {
-//         try {
-//             await driver.quit();
-//         }
-//         catch(err) {
-//             console.error(err);
-//         }
-//     });
-                                            
-//     it('renders some use case', async (done) => {
-//         // our test actions and expectations.
-
-//         // const hasTestViewElement = await driver.hasElementByAccessibilityId('testview');
-
-//         // console.log('hasTestViewElement: ', hasTestViewElement);
-
-//         // assert.equal(hasTestViewElement, true);
-
-//         const x = await driver.elementsByAndroidUIAutomator("testview");
-
-//         console.log('x: ', x);
-
-//         assert.equal(1, 1);
-
-        
-//         // expect(await driver.hasElementByAccessibilityId('testview')).toBe(true);
-//         // [...]
-//     });
-// });
